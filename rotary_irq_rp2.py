807d653732ddea0138fae295006d2ba5bf091d9a
TobiLinux <tobisc@gmail.com>
2023-01-27 20:37:58 +0000

    mod rot

--- rotary_irq_rp2.py
+++ rotary_irq_rp2.py
@@ -1,4 +1,4 @@
-# The MIT License (MIT)
+# MIT License (MIT)
 # Copyright (c) 2020 Mike Teachman
 # Copyright (c) 2021 Eric Moyer
 # https://opensource.org/licenses/MIT
@@ -22,12 +22,14 @@ class RotaryIRQ(Rotary):
         pin_num_dt,
         min_val=0,
         max_val=10,
+        incr=1,
         reverse=False,
         range_mode=Rotary.RANGE_UNBOUNDED,
         pull_up=False,
         half_step=False,
+        invert=False
     ):
-        super().__init__(min_val, max_val, reverse, range_mode, half_step)
+        super().__init__(min_val, max_val, incr, reverse, range_mode, half_step, invert)
 
         if pull_up:
             self._pin_clk = Pin(pin_num_clk, Pin.IN, Pin.PULL_UP)
@@ -66,4 +68,3 @@ class RotaryIRQ(Rotary):
 
     def _hal_close(self):
         self._hal_disable_irq()
-
