3e9c98c3fbc7f5040d060d469b94169eb6137039
TobiLinux <48185015+TobiLinux@users.noreply.github.com>
2023-05-12 18:23:57 -0300

    Update stepper.py

--- stepper.py
+++ stepper.py
@@ -1,13 +1,13 @@
 from machine import Pin
 import time
 
-pinEnabled = Pin(5, Pin.OUT,value=0)
+pinEnabled = Pin(13, Pin.OUT,value=0)
 pinStep = Pin(14, Pin.OUT)
 pinDirection = Pin(15, Pin.OUT)
 
-stepsPerRevolution = 200
-stepIntervalMs = 100 
-numCiclos = 4
+stepsPerRevolution = 51
+stepIntervalMs = 5 
+numCiclos = 2
 for i in range(numCiclos):
 
     pinDirection.on()
@@ -24,4 +24,7 @@ for i in range(numCiclos):
         pinStep.on()
         time.sleep_ms(stepIntervalMs)
         pinStep.off()
-        time.sleep_ms(stepIntervalMs)    
+        time.sleep_ms(stepIntervalMs)
+    time.sleep(2)
+
+pinEnabled.off()
